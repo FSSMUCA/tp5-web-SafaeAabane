f9380fa3abdfcff29c2692812fbb56dee1353830
SafaeAabane <s.aabane7995@uca.ac.ma>
2025-11-29 21:23:40 +0100

    lancement

--- exercice1/ex1.js
+++ exercice1/ex1.js
@@ -2,10 +2,10 @@ let x = "150";
 let y = 150;
 let z = true;
 
-console.log(typeof x);
-console.log(typeof y);
-console.log(typeof z); 
+alert(typeof x);
+alert(typeof y);
+alert(typeof z); 
 
 x = Number(x);
-console.log(x);    
-console.log(typeof x);
+alert(x);    
+alert(typeof x);
