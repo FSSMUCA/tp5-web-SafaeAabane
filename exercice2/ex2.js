e4bc1968e13fc7a96283e5e0c57bdd7c3b1786fa
SafaeAabane <s.aabane7995@uca.ac.ma>
2025-11-29 12:32:32 +0100

    lancement

--- exercice2/ex2.js
+++ exercice2/ex2.js
@@ -2,5 +2,5 @@ let prenom = "sara";
 let age = 22;
 console.log("je m'apelle" + prenom +" et j'ai " + age +"ans");
 age = 23;
-`Je m'appelle ${prenom} et j'ai ${age} ans`
+console.log(`Je m'appelle ${prenom} et j'ai ${age} ans`);
 
